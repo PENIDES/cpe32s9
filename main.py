e51f5fabacdafef6977d6c91216180997e1aa5a6
PENIDES <158258697+PENIDES@users.noreply.github.com>
2024-05-18 17:24:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import cv2
 import numpy as np
-from keras import models
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input as mobilenet_v2_preprocess_input
 
-model = models.load_model("mdl_wt.hdf5")
+model = tf.keras.models.load_model("mdl_wt.hdf5")
 
 uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
@@ -32,7 +31,7 @@ if uploaded_file is not None:
     resized = mobilenet_v2_preprocess_input(resized)
     img_reshape = resized[np.newaxis,...]
 
-    Genrate_pred = st.button("Generate Prediction")
-    if Genrate_pred:
+    Generate_pred = st.button("Generate Prediction")
+    if Generate_pred:
         prediction = model.predict(img_reshape).argmax()
-        st.title("Predicted Label for the image is {}".format(map_dict[prediction]))
+        st.title("Predicted Label for the image is {}".format(map_dict[prediction])))
